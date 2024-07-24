3d7641bd8c48ad6a06ad9b0920db7e4c28c2c15d
humanrizki <humanrizki123@gmail.com>
2024-07-25 02:14:03 +0700

    Add Editor Quill and Highlight JS

--- src/Layouts/Users/Root/Courses/Courses.jsx
+++ src/Layouts/Users/Root/Courses/Courses.jsx
@@ -1,56 +1,40 @@
-// import {
-//     // EditorBubble,
-//     // EditorBubbleItem,
-//     // EditorCommand,
-//     // EditorCommandItem,
-//     EditorContent,
-//     EditorRoot,
-// } from "novel";
-import { useEffect, useState } from "react";
-import ReactQuill from "react-quill";
+// import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
+// import hljs from "highlight.js";
+// import 'highlight.js/styles/default.css';
+import 'highlight.js/styles/atom-one-dark.css';
+import React, { useEffect, useRef, useState } from 'react';
+import Editor from './Editor';
+import { Delta } from 'quill/core';
 function Courses(){
-    const [content, setContent] = useState(null);
-    const modules = {
-        toolbar: [
-          [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
-          [{ size: [] }],
-          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-          [{ 'list': 'ordered' }, { 'list': 'bullet' },
-          { 'indent': '-1' }, { 'indent': '+1' }],
-          ['link', 'image', 'video'],
-          ['clean']
-        ],
-        clipboard: {
-          // toggle to add extra line breaks when pasting HTML:
-          matchVisual: false,
-        }
-      };
-    useEffect(()=>{
-        console.log(content)
-    },[content])
+  const [range, setRange] = useState();
+  const [lastChange, setLastChange] = useState();
+  const [readOnly, setReadOnly] = useState(false);
+  const quillRef = useRef();
+  const [content, setContent] = useState(null);
+  useEffect(()=>{console.log(content)}, [content])
   return (
     <>
-        <div className="flex justify-center items-center h-[10rem]">
-        <h1 className="text-6xl font-extrabold">Qull.Js Text Editor</h1>
-        
-
-        <ReactQuill 
-        className="h-[10rem]" 
-        theme='snow'
-        formats={['header', 'font', 'size', 'bold', 'italic', 'underline', 'strike', 'blockquote', 'list', 'bullet', 'indent', 'link', 'image', 'video']}
-        placeholder="Write something amazing..."
-        onChange={setContent} 
-        modules={modules}
-        />
-        </div>
-            <div>
-                <h1 className="text-xl font-bold flex justify-center mt-8">Preview</h1>
-                <div dangerouslySetInnerHTML={{ __html: content }}></div>
-            </div>
+      <Editor
+        ref={quillRef}
+        readOnly={readOnly}
+        defaultValue={new Delta()
+          .insert('Hello')
+          .insert('\n', { header: 1 })
+          .insert('Some ')
+          .insert('initial', { bold: true })
+          .insert(' ')
+          .insert('content', { underline: true })
+          .insert('\n')}
+        onSelectionChange={setRange}
+        onTextChange={setLastChange}
+        setContent={setContent}
+      />
+      <div>
+        <h1 className="text-xl font-bold flex justify-center mt-8">Preview</h1>
+        <div dangerouslySetInnerHTML={{ __html: content }}></div>
+      </div>
     </>
-        
-        
   )
 }
 
